e6e143632261392e9e75ea281a7327a02e389b41
JhouHank <hank57054@gmail.com>
2024-01-14 21:59:50 +0800

    fix: 🐛 測試搜尋頁翻譯

--- src/pages/search.js
+++ src/pages/search.js
@@ -8,6 +8,12 @@ export function search() {
   // 將目前項目連結 改為新分頁開啟
   // galleryBlank()
 
+  // 翻譯
+  translatePlus(['nav'], json.NewSpanPage)
+
+  // 翻譯 標籤
+  tagsTranslator($('#content > h1 > a > .name'))
+
   ajaxPage ? ajaxPageFunc() : debugConsole('自動翻頁 已關閉')
 
   function ajaxPageFunc() {
